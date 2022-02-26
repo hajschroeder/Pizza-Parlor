c29b0e063b7f39e22b317a62018d8cb1716101f2
Hans Schroeder <hajschroeder@gmail.com>
2022-02-26 11:19:15 -0800

    rewrite test 3 once more to simplify approach. Includes commented out code from previous approached

--- README.md
+++ README.md
@@ -16,13 +16,13 @@ _TEST STATUS: Passing_
 TEST 2.)
 
 1. TEST: It should inlude a price property for the constructor
-1. CODE: let firstPizza =  newPizza("medium", ["topping1", "topping2"], 0)
+1. CODE: let firstPizza =  new Pizza("medium", ["topping1", "topping2"], 0)
 1. EXPECTED OUTCOME: Pizza {size: "medium", toppings: ["topping1", "topping2"]}
 _TEST STATUS: Passing_
 
 TEST 3.)
 
-1. TEST: It should include a prototype to select the size
-1. CODE: let firstPizza = new Pizza("small", ["topping1", "topping2"])
-1. EXPECTED OUTCOME: Pizza {size: "small", "medium", "large", toppings: ["topping1", "topping2"]}
-
+1. TEST: It should assign a value to the size
+1. CODE: let firstPizza =  new Pizza("small", ["topping1", "topping2"], 0)
+1. EXPECTED OUTCOME: firstPizza //10
+_TEST STATUS: Not Passing_
--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,37 @@ function Pizza(size, toppings){
   this.toppings = toppings;
 }
 
+Pizza.prototype.pizzaPrice = function(){
+  
+}
+// Pizza.prototype.pizzaPrice = function(){
+//   let total = 0
+//   let small= {
+//     name: "small",
+//     value: 10
+//   }
+//   if (this.size = small);{
+//   total += small.value;
+//   }
+// } 
 
-let firstPizza = new Pizza("small", "topping")
+// Pizza.prototype.pizzaPrice = function(){
+//   let total = 0
+//   let pizzaSize = new Map()
+//   pizzaSize.set("small", 10)
+//   pizzaSize.set("medium", 13)
+//   pizzaSize.set("large", 15)
+
+//   this.size = pizzaSize;
+//   if (pizzaSize.has("small") === true){
+//     total += pizzaSize.get("small")
+//   } else if (pizzaSize.has("medium") === true){
+//     total += pizzaSize.get("medium")
+// } else if (pizzaSize.has("large") === true){
+//     total += pizzaSize.get("large")
+// } 
+//   console.log(total);
+// } 
+
+
 
